cd63c97264d449a1fd253ca8e26b69e241567da4
kim soojung <julieline@naver.com>
2018-12-04 19:30:54 +0900

    #CSS/js 완료, 기본 동작 구성

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
+##test
+@app.route('/user/<username>')
+def shouw_user_profile(username):
+    return 'User:  {}!!'.format(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath: {}'.format(subpath)
+
+
+@app.route('/login')
+def login():
+    return render_template('index.html')
+
+
+@app.route('/logout')
+def logout():
+    return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('index.html')
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
